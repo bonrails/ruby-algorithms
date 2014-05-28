9940f918efc25384cc72cc2017e166e7862c5912
Jagan <jreddy@book.com>
2014-05-28 15:23:24 -0700

    bubble sort implementation

---
+++ bubble_sort.rb
@@ -0,0 +1,13 @@
+class Array
+  def bubblesort!
+    each_index do |j|
+      j.upto(length - 1) do |i|
+        self[i-1], self[i] = self[i], self[i-1] if self[i-1] > self[i]
+      end
+    end
+    self
+  end
+end
+some_array = [9,4,3,5,7,8,5]
+some_array.bubblesort!
+puts some_array
