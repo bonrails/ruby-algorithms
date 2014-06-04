5bf8471d8af237ad2f3c2f029f2567151bea8cb0
reddyonrails <reddyonrails@gmail.com>
2014-06-04 09:24:30 -0700

    merge sort implementation.

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,1 +1,35 @@
 #implement merge sort
+class  Array
+
+  def merge_sort(m)
+    return m if m.length <= 1
+
+    middle = m.length / 2
+    left = m[0,middle]
+    right = m[middle..-1]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+    merge(left, right)
+  end
+
+  def merge(left, right)
+    result = []
+    until left.empty? || right.empty?
+      if left.first <= right.first
+        result << left.shift
+      else
+        result << right.shift
+      end
+    end
+    result + left + right
+  end
+
+  def merge_sort!
+    merge_sort(self)
+  end
+
+end
+
+array = [7,6,5,9,8,4,3,1,2,0]
+p array.merge_sort!
